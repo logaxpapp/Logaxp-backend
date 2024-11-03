9c14661c97e0decc23a07f644ee52736b85ea8be
Owoeye Samuel <owoeye1321@gmail.com>
2024-11-03 21:48:04 +0100

    completed mails for admin actions

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -49,6 +49,8 @@ export default interface IUserModel extends Document, IUser {
   validPassword(password: string): boolean;
   toAuthIndividualJSON(): IUserToAuthJSON;
   sendAccountDeleteRequest(email: string): void;
+  sendAccountApprovalRequest(): void;
+  sendAccountRejectionRequest(): void;
   sendWelcomeEmail(email: string, pasword: string, name: string):void
 }
 
@@ -732,6 +734,400 @@ schema.methods.sendAccountDeleteRequest = async function (email: string) {
     `;
 
   await sendMail(email, "Delete Request", body);
+};
+schema.methods.sendAccountApprovalRequest = async function () {
+    const body = `
+      <!DOCTYPE html>
+      <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="x-apple-disable-message-reformatting" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+            integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+          <title>Verify Email</title>
+          <!--[if mso]>
+            <noscript>
+              <xml>
+                <o:OfficeDocumentSettings>
+                  <o:PixelsPerInch>96</o:PixelsPerInch>
+                </o:OfficeDocumentSettings>
+              </xml>
+            </noscript>
+          <![endif]-->
+          <style>
+            body,
+            div,
+            h1,
+            p,
+            button,
+            a {
+              font-family: "Poppins", sans-serif;
+            }
+            i {
+              color: rgba(255, 255, 255, 0.9);
+            }
+            * {
+              padding: 0;
+              margin: 0;
+            }
+            p {
+              font-size: 1rem;
+            }
+            @media (max-width: 500px) {
+              p {
+                font-size: 0.875rem;
+              }
+            }
+            button:hover,
+            a:hover {
+              opacity: 0.75;
+            }
+            .main-container {
+              padding: 1.5rem;
+              margin: 1.5rem auto 0;
+              max-width: 700px;
+              width: 100%;
+              background-color: white;
+            }
+            @media (max-width: 900px) {
+              .main-container {
+                width: 80%;
+              }
+            }
+            @media (max-width: 500px) {
+              .main-container {
+                padding: 1rem;
+              }
+            }
+            .page-header {
+              color: rgba(0, 0, 0, 0.9);
+              font-weight: 600;
+              font-size: 1.5rem;
+            }
+            @media (max-width: 500px) {
+              .page-header {
+                font-size: 1.25rem;
+              }
+            }
+            .page-text {
+              color: rgba(0, 0, 0, 0.7);
+              font-weight: 400;
+            }
+            .btn-container {
+              margin: 2rem auto;
+              max-width: 300px;
+              width: 100%;
+            }
+            @media (max-width: 500px) {
+              .btn-container {
+                margin: 1.25rem auto;
+              }
+            }
+            .social-container {
+              background-color: #5cb23a;
+              padding: 1rem 1.5rem;
+              margin-top: 1.5rem;
+              border-radius: 4px;
+            }
+            @media (max-width: 500px) {
+              .social-container {
+                padding: 1rem;
+                margin-top: 1.25rem;
+              }
+            }
+            .tax-text {
+              text-align: center;
+              color: rgba(255, 255, 255, 0.9);
+              font-weight: 500;
+              width: 80%;
+              margin: auto;
+            }
+            @media (max-width: 500px) {
+              .tax-text {
+                width: 100%;
+              }
+            }
+            .social-flex {
+              margin-top: 2rem;
+              display: flex;
+              gap: 2rem;
+              align-items: center;
+              justify-content: center;
+            }
+            @media (max-width: 500px) {
+              .social-flex {
+                margin-top: 1.25rem;
+              }
+            }
+            .social-link {
+              font-size: 1.5rem;
+            }
+            .footer-link {
+              font-size: 1rem;
+              color: rgba(0, 0, 0, 0.5);
+              text-decoration: none;
+            }
+            @media (max-width: 500px) {
+              .footer-link {
+                font-size: 0.875rem;
+              }
+            }
+            .mail-text {
+              width: 80%;
+              margin: 1rem auto;
+              text-align: center;
+            }
+            @media (max-width: 500px) {
+              .mail-text {
+                width: 100%;
+              }
+            }
+          </style>
+        </head>
+        <body style="margin: 2rem 0; background-color: #f4f5fb">
+          <div style="width: 115px; height: 45px; margin: 0 auto">
+            <image
+              src=""
+              alt=" Logo"
+              style="width: 100%; height: 100%"
+            />
+          </div>
+          <div class="main-container">
+            <div>
+              <h1 class="page-header">Delete request</h1>
+              <hr style="margin: 12px 0; border: none; border-top: 1px solid rgba(0, 0, 0, 0.1)" />
+              <p class="page-text">Hello ${this.name}</p>
+              <br />
+              
+              <p class="page-text">
+              Your request has been successfully approved.
+              </p>
+            </div>
+            <br/>
+            <div>
+              <p class="page-text">
+              </p>
+              <br />
+              <p class="page-text">This is an automated message, please do not reply.</p>
+            </div>
+            <footer style="margin-top: 2rem; color: rgba(0, 0, 0, 0.5)">
+              <div style="display: flex; width: fit-content; margin: 0 auto">
+               
+              </div>
+            </footer>
+          </div>
+        </body>
+      </html>
+      `;
+  
+    await sendMail(this.email, "Request Approval", body);
+};
+schema.methods.sendAccountRejectionlRequest = async function () {
+    const body = `
+      <!DOCTYPE html>
+      <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="x-apple-disable-message-reformatting" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+            integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+          <title>Verify Email</title>
+          <!--[if mso]>
+            <noscript>
+              <xml>
+                <o:OfficeDocumentSettings>
+                  <o:PixelsPerInch>96</o:PixelsPerInch>
+                </o:OfficeDocumentSettings>
+              </xml>
+            </noscript>
+          <![endif]-->
+          <style>
+            body,
+            div,
+            h1,
+            p,
+            button,
+            a {
+              font-family: "Poppins", sans-serif;
+            }
+            i {
+              color: rgba(255, 255, 255, 0.9);
+            }
+            * {
+              padding: 0;
+              margin: 0;
+            }
+            p {
+              font-size: 1rem;
+            }
+            @media (max-width: 500px) {
+              p {
+                font-size: 0.875rem;
+              }
+            }
+            button:hover,
+            a:hover {
+              opacity: 0.75;
+            }
+            .main-container {
+              padding: 1.5rem;
+              margin: 1.5rem auto 0;
+              max-width: 700px;
+              width: 100%;
+              background-color: white;
+            }
+            @media (max-width: 900px) {
+              .main-container {
+                width: 80%;
+              }
+            }
+            @media (max-width: 500px) {
+              .main-container {
+                padding: 1rem;
+              }
+            }
+            .page-header {
+              color: rgba(0, 0, 0, 0.9);
+              font-weight: 600;
+              font-size: 1.5rem;
+            }
+            @media (max-width: 500px) {
+              .page-header {
+                font-size: 1.25rem;
+              }
+            }
+            .page-text {
+              color: rgba(0, 0, 0, 0.7);
+              font-weight: 400;
+            }
+            .btn-container {
+              margin: 2rem auto;
+              max-width: 300px;
+              width: 100%;
+            }
+            @media (max-width: 500px) {
+              .btn-container {
+                margin: 1.25rem auto;
+              }
+            }
+            .social-container {
+              background-color: #5cb23a;
+              padding: 1rem 1.5rem;
+              margin-top: 1.5rem;
+              border-radius: 4px;
+            }
+            @media (max-width: 500px) {
+              .social-container {
+                padding: 1rem;
+                margin-top: 1.25rem;
+              }
+            }
+            .tax-text {
+              text-align: center;
+              color: rgba(255, 255, 255, 0.9);
+              font-weight: 500;
+              width: 80%;
+              margin: auto;
+            }
+            @media (max-width: 500px) {
+              .tax-text {
+                width: 100%;
+              }
+            }
+            .social-flex {
+              margin-top: 2rem;
+              display: flex;
+              gap: 2rem;
+              align-items: center;
+              justify-content: center;
+            }
+            @media (max-width: 500px) {
+              .social-flex {
+                margin-top: 1.25rem;
+              }
+            }
+            .social-link {
+              font-size: 1.5rem;
+            }
+            .footer-link {
+              font-size: 1rem;
+              color: rgba(0, 0, 0, 0.5);
+              text-decoration: none;
+            }
+            @media (max-width: 500px) {
+              .footer-link {
+                font-size: 0.875rem;
+              }
+            }
+            .mail-text {
+              width: 80%;
+              margin: 1rem auto;
+              text-align: center;
+            }
+            @media (max-width: 500px) {
+              .mail-text {
+                width: 100%;
+              }
+            }
+          </style>
+        </head>
+        <body style="margin: 2rem 0; background-color: #f4f5fb">
+          <div style="width: 115px; height: 45px; margin: 0 auto">
+            <image
+              src=""
+              alt=" Logo"
+              style="width: 100%; height: 100%"
+            />
+          </div>
+          <div class="main-container">
+            <div>
+              <h1 class="page-header">Delete request</h1>
+              <hr style="margin: 12px 0; border: none; border-top: 1px solid rgba(0, 0, 0, 0.1)" />
+              <p class="page-text">Hello ${this.name}</p>
+              <br />
+              
+              <p class="page-text">
+              Your request has been rejected.
+              </p>
+            </div>
+            <br/>
+            <div>
+              <p class="page-text">
+              </p>
+              <br />
+              <p class="page-text">This is an automated message, please do not reply.</p>
+            </div>
+            <footer style="margin-top: 2rem; color: rgba(0, 0, 0, 0.5)">
+              <div style="display: flex; width: fit-content; margin: 0 auto">
+               
+              </div>
+            </footer>
+          </div>
+        </body>
+      </html>
+      `;
+  
+    await sendMail(this.email, "Request Rejected", body);
 };
 schema.methods.sendWelcomeEmail = async function (email: string, password: string, name: string) {
   const body = `
